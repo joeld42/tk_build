f050c75760314b29c9b52d618cec1d5b7a507aa3
Joel Davis <joeld42@gmail.com>
2022-03-18 20:38:47 -0700

    windows publish step

--- cfg/tapnik/civclicker_package_win.bat
+++ cfg/tapnik/civclicker_package_win.bat
@@ -1,7 +1,10 @@
 echo ON
-mkdir %1\civclicker
+
+mkdir %1\civclicker_dist
+
+xcopy /y %1\..\fips-deploy\%2\win64-vs2017-release\civclicker.exe %1\civclicker_dist\
+xcopy /y %1\README.md %1\civclicker_dist\
+robocopy /s /e %1\gamedata %1\civclicker_dist\gamedata
+
 cd %1
-REM xcopy build\Release\puzzle3.exe .\civclicker\
-REM xcopy README.md .\testrepo\
-REM c:\Toolkits\7-Zip\7z a -tzip civclicker_%2_build_%3.zip testrepo
-echo TODO: Will package civclicker_%2_build_%3.zip
+c:\Toolkits\7-Zip\7z a -tzip civclicker_%3_build_%4.zip civclicker_dist
